9e29cccb2ac4ed7081e6f284d3bba88ffc900774
Taylor Kline <taylor.kline@streamingfreebies.com>
2017-11-12 19:40:58 -0600

    Refactor disqualification to one iteration of authors

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ import datetime
 SUBMISSION_ID = ""
 NUM_WINNERS = 10
 MIN_ACCOUNT_AGE_DAYS = 7
+MIN_KARMA = 10
+MIN_COMMENTS = 5
 
 
 def main():
@@ -20,14 +22,14 @@ def main():
     print()
 
     mods = set(m for m in submission.subreddit.moderator())
-    authors = remove_mods(authors, mods)
+    disqualified = dict()
+    choose_disqualified(authors, mods, disqualified)
 
-    authors = remove_new(authors)
-
-    # TODO: Further disqualification / pruning criteria
-    # Such as:
-    # Combined comment + submission karma < 10
-    # Number of submissions + comments < 10
+    authors = authors - disqualified.keys()
+    print(f"{len(authors)} authors remain qualified after the folllowing were removed:")
+    for author, disqualification_reason in sorted(disqualified.items(), key=lambda x: x[0].name):
+        print(f"{author.name} - {disqualification_reason}")
+    print()
 
     print(f"The folllowing {len(authors)} users remain qualified:")
     print("\n".join(sorted(a.name for a in authors)))
@@ -41,33 +43,36 @@ def main():
     for winner in winners:
         assert(winner in authors)
         assert(winner not in mods)
+        assert(winner not in disqualified)
 
 
-def remove_mods(authors, mods):
-    authors_minus_mods = authors - mods
-    mods_removed = authors & mods
-    print(
-        f"There are now {len(authors_minus_mods)} authors after the following mods were removed:")
-    print(f"\n".join(sorted((m.name for m in mods_removed))))
-    print()
-    return authors_minus_mods
-
-
-def remove_new(authors):
-    new_authors = set()
+def choose_disqualified(authors, mods, disqualified):
+    now = datetime.date.today()
     for author in authors:
-        now = datetime.date.today()
+        # Prune mods
+        if author in mods:
+            disqualified[author] = "Is a moderator of the subreddit."
+            continue
+
+        # Prune accounts under min age
         account_created = datetime.date.fromtimestamp(author.created_utc)
         age = now - account_created
         assert(age.days >= 0)
         if age.days < MIN_ACCOUNT_AGE_DAYS:
-            new_authors.add(author)
-    authors_minus_new = authors - new_authors
-    print(
-        f"Removed {len(new_authors)} authors due to an account age < {MIN_ACCOUNT_AGE_DAYS} days:")
-    print("\n".join(sorted(a.name for a in new_authors)))
-    print()
-    return authors_minus_new
+            disqualified[author] = f"Account is only {age.days} old."
+            continue
+
+        # Prune accounts with low karma
+        if author.comment_karma + author.link_karma < MIN_KARMA:
+            disqualified[author] = f"Combined link and comment karma is {author.comment_karma + author.link_karma} versus cutoff of {MIN_KARMA}."
+            continue
+
+        # Prune accounts with fewer than the min number of comments
+        comment_count = len(list(author.comments.new(limit=MIN_COMMENTS + 5))) + \
+            len(list(author.submissions.new(limit=MIN_COMMENTS + 5)))
+        if comment_count < MIN_COMMENTS:
+            disqualified[author] = f"Author has only made {comment_count} versus the cutoff of {MIN_COMMENTS}."
+            continue
 
 
 def authenticate():
